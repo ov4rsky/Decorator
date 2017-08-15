74b8da4df20dae69bb912885a8bf6c95ca92c534
ov4rsky <30823933+ov4rsky@users.noreply.github.com>
2017-08-15 17:03:41 +0700

    Add   DarkRoset Class +Decaf Class + Soy Class + SteamedMilk Class

---
+++ src/DarkRoset.java
@@ -0,0 +1,14 @@
+
+public class DarkRoset extends Beverage {
+	public DarkRoset(){
+    	description = "DarkRoset";
+    }
+	@Override
+	public double cost() {
+		
+		// TODO Auto-generated method stub
+		
+		return 0.99;
+	}
+
+}
